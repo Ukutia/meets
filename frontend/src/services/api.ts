e56ded813f6e1e0767ccad8fcae4c0427145b053
Ukutia <118562759+Ukutia@users.noreply.github.com>
2026-02-23 23:36:29 -0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,7 @@ import type {
   Proveedor
 } from '@/types';
 
-const API_BASE_URL = import.meta.env.BACKEND_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/apaaaaaaaaaaaai';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
